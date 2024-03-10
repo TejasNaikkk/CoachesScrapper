a38b199121dfc84ded9819548cabb4ffcf930106
Tejas Naik <tejasnaik121@gmail.com>
2024-03-10 19:17:15 +0530

    gets individual user profile link at this point

--- main.py
+++ main.py
@@ -5,8 +5,21 @@ import config
 
 base_url = config.url
 
-links = []
-for i in range(1,10):
-    page = i
-    url = f'{base_url}{page}'
-    links.append(url)
+
+page = 1
+url = f'{base_url}{page}'
+soup = souper(url)
+section = soup.find('section', class_='directory').find_all('div', class_= 'avatar')
+a = []
+for div in section:
+    As = div.find_all('a')
+    for a in As:
+        href = a['href']
+        print(href)
+# div = section.find_all('div', class_ = 'coach')
+# print(div)
+# for a_tag in a_tags:
+#     href_value = a_tag['href']
+#     print(href_value)
+
+
