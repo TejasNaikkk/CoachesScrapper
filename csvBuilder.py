62c17f1316e8adb524de69a59d332b5a85176546
Tejas Naik <tejasnaik121@gmail.com>
2024-03-12 01:48:31 +0530

    csv Builder file added

---
+++ csvBuilder.py
@@ -0,0 +1,31 @@
+import csv
+import config
+# from datetime import datetime
+
+opFolder = config.output_path
+# now = str(datetime.now())
+
+
+# csvFileName = f'{opFolder}{now}.csv'
+
+csvFileName = f'{opFolder}test.csv'
+
+
+
+
+def makeCSV(payload):
+    # Check if the CSV file exists, if not, write header
+    try:
+        with open(csvFileName, 'x', newline='') as csvfile:
+            fieldnames = payload.keys()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow(payload)
+    except FileExistsError:
+        pass
+
+    # Write the payload to the CSV file
+    with open(csvFileName, 'a', newline='') as csvfile:
+        fieldnames = payload.keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(payload)
